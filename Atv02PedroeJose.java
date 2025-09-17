9f74c5b40f4be6202678adddd93c28c9ee844c84
gabrielborges1990br-prog <gabrielborges1990br@gmail.com>
2025-09-16 21:02:18 -0300

    Add files via upload

---
+++ Atv02PedroeJose.java
@@ -0,0 +1,9 @@
+package com.mycompany.atv02pedroejose;
+
+public class Atv02PedroeJose {
+
+    public static void main(String[] args) {
+        System.out.print("Ola Mundo");
+        System.out.println("Ola Mundo");
+    }
+}
