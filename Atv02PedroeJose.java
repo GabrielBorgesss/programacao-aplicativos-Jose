e337958d9f0cbaf1e2dc3398e8367328639c16db
gabrielborges1990br-prog <gabrielborges1990br@gmail.com>
2025-09-16 21:04:20 -0300

    Delete Atv02PedroeJose.java

--- Atv02PedroeJose.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.atv02pedroejose;
-
-public class Atv02PedroeJose {
-
-    public static void main(String[] args) {
-        System.out.print("Ola Mundo");
-        System.out.println("Ola Mundo");
-    }
-}
